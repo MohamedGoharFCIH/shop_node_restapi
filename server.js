90edf6c55372501a4f68e88b19428fe233babdcf
MohamedGoharFCIH <engmabdo85@gmail.com>
2022-08-31 07:20:51 +0200

    create express server

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const sequelize = require("./database/db")
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator')
+const cors = require('cors')
+const userRoutes = require('./routes/users');
+const orderRoutes = require("./routes/orders")
+
 
 const User = require("./models/user")
 const Product = require("./models/product")
@@ -25,12 +33,35 @@ Product.belongsToMany(Order, {
 });
 
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(expressValidator())
 
-sequelize.sync().then(() => {
-    console.log('Connection has been established successfully and databases models created.');
-}).catch((error) => {
-    console.error('Unable to connect to the database: ', error);
+app.use(cors());
+
+app.use('/api/user', userRoutes);
+app.use('/api/order', orderRoutes);
+
+app.get('/', (req, res) => {
+    res.send('hello Server running on localhost: 3000');
+});
+
+app.get('*', (req, res) => {
+    res.send('Error 404 ...! Page Not Found');
 });
 
 
+sequelize.sync()
+    .then(() => {
+        // app.listen(3000, () => {
+        //     console.log("server running on localhost:3000")
+        // });
+
+        // console.log('Connection has been established successfully and databases models created.');
+    }).catch((error) => {
+        console.error('Unable to connect to the database: ', error);
+    });
+
+module.exports = app.listen(3000)
 
