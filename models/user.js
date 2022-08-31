0e87e24593df16e38ae3d29fe9802b82b08301e6
MohamedGoharFCIH <engmabdo85@gmail.com>
2022-08-31 07:26:09 +0200

    add unique constrains to email in user model

--- models/user.js
+++ models/user.js
@@ -32,8 +32,10 @@ const User = sequelize.define('user', {
     email: {
         type: Sequelize.STRING,
         allowNull: false,
+        unique: true,
         validate: {
-            isEmail: true
+            isEmail: true,
+            
         }
     },
     password: {
